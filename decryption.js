def3a4c6ca4f29e87b2833d26491bce67170b732
arunkumar <arunkumar.s@bridgei2i.com>
2017-12-14 17:57:16 +0530

    js files for ajax call and modifications in html files

---
+++ decryption.js
@@ -0,0 +1,21 @@
+//Ajax call for Decryption
+function decryptMessage(){
+    var url_string = window.location.href;
+    var clean_uri = location.protocol + "//" + location.host + location.pathname;
+    window.history.replaceState({}, document.title, clean_uri);
+    var url = new URL(url_string);
+    var message = url.searchParams.get("message");
+    var product = localStorage.getItem("product");
+    $.ajax({
+        url :'https://pjaqzs646c.execute-api.ap-south-1.amazonaws.com/tangle/decrypt?message=' + message + "&product=" + product,
+        type : 'GET',
+        dataType: 'html',
+        data : {"message":message, "product":product},
+        success : function(resp){
+            result = $.parseJSON(data);
+            decrypted_message = result['decrypted_message'];
+            $("#encrypted-text").val(message);
+            $("#decrypted-text").val(decrypted_message);
+        }
+    });
+}
---
+++ encryption.js
@@ -0,0 +1,50 @@
+var product = $('#product').text();
+cropped_digits = String($('#product').text());
+
+//Check the range of product
+function rangeKeyUp(){
+    cropped_digits = String($('#product').text());
+    if($("#prodrange-from").val() == -8 && $("#prodrange-to").val() == '')
+        {
+            cropped_digits = cropped_digits.slice($("#prodrange-from").val());
+        }else{
+            cropped_digits = cropped_digits.slice($("#prodrange-from").val(), $("#prodrange-to").val());
+        }
+    $("#digits").text(cropped_digits);
+    if(cropped_digits.length < 8){
+        $("#alert").show();
+    }else{
+        $("#alert").hide();
+    }
+}
+
+
+function replaceStr(str, pos, value){
+    var arr = str.split('');
+    arr[pos]=value;
+    return arr.join('');
+}
+
+//Ajax call for Encryption
+$("#encrypt-btn").click(function(){
+    var message = $("#plain-text").val();
+    var product = String($("#digits").text());
+    product = replaceStr(product, 0, '1');
+    product = replaceStr(product, 1, '0');
+    console.log(product);
+    localStorage.setItem("product", product);
+    $.ajax({
+        url :'https://pjaqzs646c.execute-api.ap-south-1.amazonaws.com/tangle/encrypt?message=' + message + "&product=" + product,
+        type : 'GET',
+        dataType: 'html',
+        success : function(resp){
+            result = $.parseJSON(resp);
+            encrypted_message = result['result'];
+            alert(encrypted_message);
+            window.open(
+                'http://alice.runcy.me/bob.html?message=' + encrypted_message,
+                '_blank' // <- This is what makes it open in a new window.
+              );
+        }
+    });
+})
